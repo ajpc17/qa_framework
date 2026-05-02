8df92d91054c8b3587acf296e8471b51332ad82f
ajpc17 <ajpc17@gmail.com>
2026-05-01 22:54:37 -0400

    fase 4 - configuracion flexible con config.py

---
+++ config/config.py
@@ -0,0 +1,3 @@
+BASE_URL = "https://www.google.com"
+BROWSER = "chrome"
+WAIT_TIME = 10
--- pages/google_page.py
+++ pages/google_page.py
@@ -1,13 +1,15 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from config.config import BASE_URL, WAIT_TIME
 
 class GooglePage:
     def __init__(self, driver):
         self.driver = driver
+        self.wait = WebDriverWait(driver, WAIT_TIME)
 
     def open(self):
-        self.driver.get("https://www.google.com")
+        self.driver.get(BASE_URL)
 
     def search(self, text):
         box = self.driver.find_element("name", "q")
