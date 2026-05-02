414af235897b22390e990d3c8a340be6da29542b
ajpc17 <ajpc17@gmail.com>
2026-05-01 23:32:37 -0400

    fix - modo headless para CI/CD en Linux

--- utils/driver.py
+++ utils/driver.py
@@ -1,6 +1,13 @@
+import os
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 def get_driver():
-    driver = webdriver.Chrome()
+    options = Options()
+    if os.environ.get("HEADLESS"):
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+    driver = webdriver.Chrome(options=options)
     driver.maximize_window()
     return driver
