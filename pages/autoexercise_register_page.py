fb3dc2fd4a5a313efa9d623efae8678c867149e2
ajpc17 <ajpc17@gmail.com>
2026-05-24 20:32:24 -0400

    tests registro automationexercise

---
+++ pages/autoexercise_register_page.py
@@ -0,0 +1,33 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from config.config import WAIT_TIME
+
+class AutoexerciseRegisterPage:
+    URL = "https://automationexercise.com/login"
+
+    def __init__(self, driver):
+        self.driver = driver
+        self.wait = WebDriverWait(driver, WAIT_TIME)
+
+    def open(self):
+        self.driver.get(self.URL)
+
+    def registrar(self, nombre, email):
+        self.driver.find_element(
+            By.CSS_SELECTOR, "[data-qa='signup-name']"
+        ).send_keys(nombre)
+        self.driver.find_element(
+            By.CSS_SELECTOR, "[data-qa='signup-email']"
+        ).send_keys(email)
+        self.driver.find_element(
+            By.CSS_SELECTOR, "[data-qa='signup-button']"
+        ).click()
+
+    def email_ya_existe(self):
+        return self.wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, ".signup-form p"))
+        ).text
+
+    def redirige_a_registro(self):
+        return "signup" in self.driver.current_url
---
+++ tests/test_autoexercise_register.py
@@ -0,0 +1,26 @@
+import pytest
+import time
+from pages.autoexercise_register_page import AutoexerciseRegisterPage
+
+def test_registro_email_existente(driver):
+    page = AutoexerciseRegisterPage(driver)
+    page.open()
+    page.registrar("TestQA", "testqa.farma@gmail.com")
+    assert "Email Address already exist!" in page.email_ya_existe()
+
+def test_registro_nuevo_usuario_redirige(driver):
+    page = AutoexerciseRegisterPage(driver)
+    page.open()
+    page.registrar("TestQA", f"testqa{int(time.time())}@gmail.com")
+    assert page.redirige_a_registro()
+
+@pytest.mark.parametrize("nombre,email,esperado", [
+    ("TestQA", "testqa.farma@gmail.com", "already exist"),
+    ("Nuevo User",  f"nuevo{int(time.time())}@gmail.com", "signup"),
+   
+])
+def test_registro_data_driven(driver, nombre, email, esperado):
+    page = AutoexerciseRegisterPage(driver)
+    page.open()
+    page.registrar(nombre, email)
+    assert esperado in driver.page_source
